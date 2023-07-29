fcde93df1525093aa96f00943ec7dac12d72b9d1
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-07-29 11:25:18 +0700

    fix: fix datasource source

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,16 +1,20 @@
 import { DataSource } from 'typeorm';
 import dotEnv from 'dotenv';
+import fs from 'fs'
 
 dotEnv.config()
 export const dataSource = new DataSource({
     type: 'postgres',
-    url: process.env.POSTGRES_URI,
-    ssl: true,
-    // host: process.env.DB_HOST,
-    // port: 5432,
-    // username: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
+    // url: process.env.POSTGRES_URI,
+    // ssl: {
+    //     rejectUnauthorized: true,
+    //     ca: fs.readFileSync('prod-ca-2021.crt').toString(),
+    //     },
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: ["src/models/*.model.ts"],
     migrations: ['src/database/migrations/*.{js,ts}'],     
     // migrationsRun: true,
